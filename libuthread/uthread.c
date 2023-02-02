003099516b19aad7c6164c765ffacee75cbe4649
Dominic Quintero <dominicaq09@gmail.com>
2023-02-01 23:06:29 -0800

    Semi-Pseudo uthread logic

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -8,10 +8,39 @@
 
 #include "private.h"
 #include "uthread.h"
+#include "queue.h"
 
-struct uthread_tcb {
-	/* TODO Phase 2 */
-};
+// Thread states
+// ============================================================================
+// TODO: More than likely too many states
+#define READY	0
+#define BLOCKED	1
+#define RUNNING 2
+#define ZOMBIE 	3
+#define IDLE	4
+#define EXITED  5
+/* State Notes
+* When a thread leaves the ready queue, its running
+* Running state -> Blocked || Zombie || Ready
+* Blocked State -> Ready
+* Zombie State  -> Process Collection
+* Ready State 	-> Process Created
+* Idle State 	-> In a infinite loop, changes when exited loop
+*/
+
+// Thread struct
+// ============================================================================
+typedef struct uthread_tcb {
+	int state;		   // Current thread state
+	uthread_ctx_t ctx; // Thread context
+	void *stack_head;  // Stack
+}uthread_tcb;
+
+// Scheduler
+// ============================================================================
+queue_t 	READY_QUEUE;
+queue_t 	BLOCKED_QUEUE;
+uthread_tcb *CURR_THREAD;
 
 struct uthread_tcb *uthread_current(void)
 {
@@ -26,6 +55,8 @@ struct uthread_tcb *uthread_current(void)
  */
 void uthread_yield(void) {
 	/* TODO Phase 2 */
+	CURR_THREAD->state = BLOCKED;
+	queue_enqueue(BLOCKED_QUEUE, CURR_THREAD);
 }
 
 /*
@@ -39,6 +70,7 @@ void uthread_yield(void) {
 void uthread_exit(void)
 {
 	/* TODO Phase 2 */
+	// Kill the CURR_THREAD
 }
 
 /*
@@ -53,8 +85,22 @@ void uthread_exit(void)
  * context creation).
  */
 int uthread_create(uthread_func_t func, void *arg) {
-	/* TODO Phase 2 */
-	// Start here
+	uthread_tcb *new_thread = malloc(sizeof(uthread_tcb));
+	if (new_thread == NULL) {
+		// ERROR: Bad malloc
+		return -1;
+	}
+
+	// Initialize thread
+	new_thread->state = READY;
+	new_thread->stack_head = uthread_ctx_alloc_stack();
+	int retval = uthread_ctx_init(&new_thread->ctx, new_thread->stack_head, func, arg);
+	if (retval <= -1) {
+		// ERROR: Init context failed
+		return -1;
+	}
+
+	queue_enqueue(READY_QUEUE, new_thread);
 	return 0;
 }
 
@@ -73,9 +119,32 @@ int uthread_create(uthread_func_t func, void *arg) {
  * Return: 0 in case of success, -1 in case of failure (e.g., memory allocation,
  * context creation).
  */
-int uthread_run(bool preempt, uthread_func_t func, void *arg)
-{
-	/* TODO Phase 2 */
+int uthread_run(bool preempt, uthread_func_t func, void *arg) {
+	// Set current processes single thread as idle
+	// to schedule later for execution
+	CURR_THREAD->state = IDLE;
+	queue_enqueue(BLOCKED_QUEUE, CURR_THREAD);
+	
+	// Create a new thread (initial_thread)
+	int retval = uthread_create(func, arg);
+	if (retval <= -1) {
+		// ERROR: Thread creation failed
+		return -1;
+	}
+
+	// Execute a infinite loop
+	while (1) {
+		// - when there are no more threads ready to run, stop the idle loop and return
+		int len = queue_length(READY_QUEUE)
+		if (len == 0) {
+			return 0;
+		} else {
+			// - or yield to next available thread
+		}
+		
+		
+		// - or deal with threads that reach completion and destroys their TCB
+	}
 }
 
 void uthread_block(void)
