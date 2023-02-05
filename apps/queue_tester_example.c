d081672acc5fa3f7dcd278f8206236b8e12ba508
Dominic Quintero <dominicaq09@gmail.com>
2023-02-05 11:18:07 -0800

    Queue test WIP

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -20,8 +20,9 @@ do {									\
 	}									\
 } while(0)
 
-// Callbacks
+// Callbacks / Misc functions
 // ============================================================================
+/* Delete a node that has 42 */
 static void iterator_inc(queue_t q, void *data) {
     int *a = (int*)data;
 
@@ -31,16 +32,34 @@ static void iterator_inc(queue_t q, void *data) {
         *a += 1;
 }
 
-static void print_queue(queue_t q, void *data) {
+/* Print entire queue */
+static void print_int(queue_t q, void *data) {
 	int *a = (int*)data;
 	if (a != NULL) {
-		printf("Queue_list: %d\n", *(int*)a);
+		printf(" [%d] ", *(int*)a);
 	}
 }
 
-// Misc functions
-// ============================================================================
-/* Free queue */
+static void print_char(queue_t q, void *data) {
+	char *a = (char*)data;
+	if (a != NULL) {
+		printf(" [%c] ", *(char*)a);
+	}
+}
+
+/* Format print */
+void print_queue(queue_t q, int is_int) {
+	printf("Queue:");
+	if (is_int) {
+		queue_iterate(q, print_int);
+	} else {
+		queue_iterate(q, print_char);
+	}
+	
+	printf("\n");
+}
+
+/* Free entire queue */
 void free_queue(queue_t q) {
 	int *ptr;
 	int queue_len = queue_length(q);
@@ -63,8 +82,7 @@ void test_create(void) {
 }
 
 /* Enqueue/Dequeue simple */
-void test_queue_simple(void)
-{
+void test_queue_simple(void) {
 	int data = 3, *ptr;
 	queue_t q = queue_create();
 
@@ -125,8 +143,28 @@ void test_enqueue_delete() {
 	fprintf(stderr, "*** TEST enqueue_delete ***\n");
 	queue_enqueue(q, &data);
 	queue_enqueue(q, &data);
+	print_queue(q, 1);
 
 	queue_iterate(q, iterator_inc);
+	TEST_ASSERT(queue_length(q) == 0);
+	free_queue(q);
+}
+
+/* Mass insertions / deletes */
+void test_rigirous(){
+	queue_t q = queue_create();
+	int num_itterations = 1000;
+	int data = 24;
+	int *ret;
+
+	// Expect no memory leaks
+	fprintf(stderr, "*** TEST rigirous ***\n");
+	for (int i = 0; i < num_itterations; ++i) {
+		queue_enqueue(q, &data);
+		for (int j = 0; j < num_itterations; ++j) {
+			queue_dequeue(q, (void**)&ret);
+		}
+	}
 	TEST_ASSERT(queue_length(q) == 0);
 	free_queue(q);
 }
@@ -137,6 +175,7 @@ int main(void) {
 	test_len();
 	test_iterator();
 	test_enqueue_delete();
+	test_rigirous();
 
 	return 0;
 }
